2f24573659780221b15336d54e87c7664dc34b33
DwanL <253929992@qq.com>
2024-08-15 11:36:13 +0800

    fix: chage dir doc to docs

--- packages/preview/vite.config.ts
+++ packages/preview/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: path.resolve(__dirname, "../../doc"),
+    outDir: path.resolve(__dirname, "../../docs"),
 
   }
 })
